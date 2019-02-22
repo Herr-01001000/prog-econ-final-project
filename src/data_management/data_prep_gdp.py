4de4da47cf4fa99d81395c8c2901e8a3a3aac6f5
Herr.01001000 <s6wehuuu@uni-bonn.de>
2019-02-22 19:07:29 +0100

    Add docstrings and adjust the code style to fit PEP8.

--- src/data_management/data_prep_gdp.py
+++ src/data_management/data_prep_gdp.py
@@ -1,4 +1,10 @@
-"""
+"""The file "data_prep_gdp.py" cleans the data from
+SAGDP2N__ALL_AREAS_1997_2017.csv. It prepares the dataset we use for
+further analysis.
+
+Data source: U.S. Department of Commerce / Bureau of Economic Analysis /
+Regional Economic Accounts
+
 """
 
 
@@ -14,28 +20,29 @@ def save_data(sample):
 
 
 if __name__ == "__main__":
-    state_names = json.load(open(ppj("IN_MODEL_SPECS", "state_names.json"), encoding="utf-8"))
+    state_names = json.load(open(ppj("IN_MODEL_SPECS", "state_names.json"),
+                                 encoding="utf-8"))
     data = pd.read_csv(ppj("IN_DATA", "SAGDP2N__ALL_AREAS_1997_2017.csv"))
-    
+
     excluded_years = [str(i) for i in list(range(1997, 2007, 1))]
     data.drop(columns=excluded_years, inplace=True)
     data.drop(columns=['GeoFIPS', 'Region', 'TableName', 'ComponentName',
-        'Unit', 'IndustryClassification', 'Description'], inplace=True)
-    
+                       'Unit', 'IndustryClassification', 'Description'],
+              inplace=True)
+
     areas = data['GeoName'].unique().tolist()
     for area in areas[:]:
         if area in state_names['excluded_area']:
             areas.remove(area)
-    data = data[(data.GeoName.isin(areas)) & (data.IndustryId==1)]
+    data = data[(data.GeoName.isin(areas)) & (data.IndustryId == 1)]
 
     data.replace(state_names, inplace=True)
-    
+
     data = pd.wide_to_long(data, ['20'], i='GeoName', j='year').reset_index()
-    
-    data.rename(columns={'GeoName':'state', '20':'gdp'}, inplace=True)    
+
+    data.rename(columns={'GeoName': 'state', '20': 'gdp'}, inplace=True)
     data['year'] = data['year'].astype(np.int) + 2000
     data['gdp'] = data['gdp'].astype(np.float)
     data.drop(columns=['IndustryId'], inplace=True)
 
-    
     save_data(data)
--- src/data_management/data_prep_pi.py
+++ src/data_management/data_prep_pi.py
@@ -1,4 +1,10 @@
-"""
+"""The file "data_prep_pi.py" cleans the data from
+SAINC1_1998_2017_ALL_AREAS.csv. It prepares the dataset we use for
+further analysis.
+
+Data source: U.S. Department of Commerce / Bureau of Economic Analysis /
+Regional Economic Accounts
+
 """
 
 
@@ -14,8 +20,10 @@ def save_data(sample):
 
 
 if __name__ == "__main__":
-    state_names = json.load(open(ppj("IN_MODEL_SPECS", "state_names.json"), encoding="utf-8"))
-    data = pd.read_csv(ppj("IN_DATA", "SAINC1_1998_2017_ALL_AREAS.csv"), encoding="ISO-8859-1")
+    state_names = json.load(open(ppj("IN_MODEL_SPECS", "state_names.json"),
+                                 encoding="utf-8"))
+    data = pd.read_csv(ppj("IN_DATA", "SAINC1_1998_2017_ALL_AREAS.csv"),
+                       encoding="ISO-8859-1")
 
     excluded_years = [str(i) for i in list(range(1998, 2007, 1))]
     data.drop(columns=excluded_years, inplace=True)
@@ -33,12 +41,19 @@ if __name__ == "__main__":
     data.replace(r'Alaska*', 'AK', inplace=True)
     data.replace(r'Hawaii*', 'HI', inplace=True)
 
-    data = pd.wide_to_long(data, ['20'], i=['GeoName','LineCode'], j='year').reset_index()
-    data = data.set_index(['GeoName', 'year', 'LineCode']).unstack().reset_index()
+    data = pd.wide_to_long(data,
+                           ['20'],
+                           i=['GeoName', 'LineCode'],
+                           j='year').reset_index()
+    data = data.set_index(['GeoName',
+                           'year',
+                           'LineCode']).unstack().reset_index()
     data.columns = data.columns.map('{0[0]}{0[1]}'.format)
-    
-    data.rename(columns={'GeoName': 'state', '201.0': 'pi',
-                         '202.0': 'population', '203.0': 'pi_pc'}, inplace=True)
+
+    data.rename(columns={'GeoName': 'state',
+                         '201.0': 'pi',
+                         '202.0': 'population',
+                         '203.0': 'pi_pc'}, inplace=True)
     data['year'] = data['year'].astype(np.int) + 2000
     data['income'] = data['pi_pc'] / 1000
     data['L_income'] = data[['income', 'state']].groupby('state').shift(1)
