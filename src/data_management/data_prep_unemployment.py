853be62ef4ff20e29f0f32a43ebd37734907fb44
Herr.01001000 <s6wehuuu@uni-bonn.de>
2019-02-24 02:52:44 +0100

    Add preparation code for unemployment data from Bureau of Labor Statistics.

---
+++ src/data_management/data_prep_unemployment.py
@@ -0,0 +1,36 @@
+"""The file "data_prep_unemployment.py" leans the data from staadata.xlsx.
+It prepares internet use data for further analysis..
+
+Data source: Bureau of Labor Statistics, https://www.bls.gov/lau/rdscnp16.htm
+
+"""
+
+
+import numpy as np
+import pandas as pd
+import json
+
+from bld.project_paths import project_paths_join as ppj
+from src.model_code.data_cleaner import data_cleaner, lag_generator
+
+
+def save_data(sample):
+    """Save cleaned data as .dta file.
+    """
+    sample.to_stata(ppj("OUT_DATA", "unemployment.dta"))
+
+
+if __name__ == "__main__":
+    state_names = json.load(open(ppj("IN_MODEL_SPECS", "state_names.json"),
+                                 encoding="utf-8"))
+    data = pd.read_excel(ppj("IN_DATA", "staadata.xlsx"))
+
+    data = data.iloc[:, [1, 2, 9]].dropna()
+    data.columns = ['state', 'year', 'unemployment']
+    data['year'] = data['year'].astype(np.int)
+    data['unemployment'] = data['unemployment'].astype(np.float)
+
+    data = data_cleaner(data[data.year > 2006], 'state', state_names)
+    data = lag_generator(data, 'unemployment', 'year', 'state')
+
+    save_data(data)
